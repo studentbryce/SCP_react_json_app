0cdf857ed65ab78a7c7908b632be275c3769dd9c
Bryce Milbank <student.bryce@hotmail.com>
2025-03-18 20:11:53 +1300

    update Navbar Homepage link

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = ({ data, onSelect }) => {
       <ul>
         {/* Add Home link */}
         <li>
-        <button onClick={() => window.location.href = "/"}>Home</button>
+        <button onClick={() => window.location.href = "/SCP_react_json_app/"}>Home</button>
         </li>
         {data.map((item, index) => (
           <li key={index}>
